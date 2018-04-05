0b0744a249c6d79e92156842c6e701fa489d8d8a
piotrjankiewicz <piotr.jankiewicz@movecloser.pl>
2018-04-05 18:47:26 +0200

    index view

--- client/index.js
+++ client/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
+
+
 import './styles/index.scss';
 import App from './components/App';
+import Edit from './components/meeting/Edit';
+import Create from './components/meeting/Create';
+import Show from './components/meeting/Show';
 
 window.$ = window.jQuery = require('jquery'); // required for bootstrap
 window.Popper = require('popper.js'); // required for tooltip, popup...
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route exact path='/' component={App}/>
+            <Route path='/edit/:id' component={Edit}/>
+            <Route path='/create' component={Create}/>
+            <Route path='/show/:id' component={Show}/>
+        </div>
+    </Router>
+    , document.getElementById('root')
+);
