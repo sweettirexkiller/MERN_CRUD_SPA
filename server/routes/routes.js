28f9e9452cd997b64895ff96c8b479471664fbf6
piotrjankiewicz <piotr.jankiewicz@movecloser.pl>
2018-04-05 15:37:05 +0200

    endpoints done

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -41,5 +41,20 @@ router.route('/update')
         });
     });
 
+router.get('/delete', function (req, res) {
+    var id = req.query.id;
+    Event.find({_id: id}).remove().exec(function(err,event){
+        if(err) res.send(err);
+        res.send('Event successfully deleted!');
+    })
+});
+
+router.get('/getAll', function(req,res){
+   Event.find({}, function(err, events){
+       if(err) res.send(err);
+       res.json(events);
+   })
+});
+
 
 module.exports = router;
