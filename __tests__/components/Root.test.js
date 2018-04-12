cb6df29ea3b6be52a511d1a5a841bb14ad97a223
sweettirexkiller <piotrjankiewicz02@gmail.com>
2018-04-12 16:28:26 +0200

    testing with reactDom not shallow enzyme ;)

--- __tests__/components/Root.test.js
+++ __tests__/components/Root.test.js
@@ -3,11 +3,14 @@
  */
 
 import React from 'react';
+import ReactDom from 'react-dom';
 import Root from '../../client/components/Root';
 import { shallow } from 'enzyme';
 
 describe('<Root/>', ()=>{
-    it('renders 1 <Root /> component', ()=>{
+    it('it renders  <Root /> without crashing', ()=>{
+        // const div = document.createElement('div');
+        // ReactDom.render(<Root/>, div);
         const component = shallow(<Root/>);
         expect(component).toHaveLength(1);
     })
