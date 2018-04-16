ec7a1afc6b630ef8457fd0bab2ce34e569ada754
sweettirexkiller <piotrjankiewicz02@gmail.com>
2018-04-16 14:41:40 +0200

    plan for testscomplete"

--- client/components/meeting/All.test.js
+++ client/components/meeting/All.test.js
@@ -8,14 +8,27 @@ import reducer from '../../store/reducers/index';
 
 const middlewares = [thunk];
 
-const mockStore = configureMockStore(reducer,middlewares);
+const mockStore = configureMockStore(reducer, middlewares);
 import initialState from '../../store/initialState';
 
 describe('<All/>', () => {
+
 
     it('renders without crashing', () => {
         const store = mockStore(initialState);
         const component = mount(<PureAll store={store}/>);
         expect(component).toBeTruthy();
+    });
+
+    it('fetches meetings from store on componentDidMount', () => {
+        //mock axios for getting the meetings
+    });
+
+    it('it renders loading component when fetching meetings', ()=>{
+        //pass the prop of fetching as true, check is the component exists (find it)
+    });
+
+    it('maps through all meetings and displays them in the table', ()=>{
+        //find them
     });
 });
